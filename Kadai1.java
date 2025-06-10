49c86557f2588e496b2f97de513bd11878f723cc
302A-01\cre <a>
2025-06-10 15:13:42 +0900

    kaidai1 コマンドライン引数に文字を入れてもエラーを吐かないように

--- Kadai1.java
+++ Kadai1.java
@@ -1,7 +1,11 @@
-public class Kadai1{
-   public static void main(String[] args){
-//       for(int n = 1; n <= 1000;n++) {
-//           System.out.print(n + " ");
-       }
-}
+public class Kadai1 {
+    public static void main(String[] args) {
+        int n = Integer.parseInt(args[0]);
+        for (int i = 1; i <= 1000; i++) {
+            System.out.print(i + " ");
+            if(i%n == 0) {
+                System.out.println();
+            }
+        }
+    }
 }
