7e79a1d9030ee6cea4a16167ea27703e718e6e98
302A-01\cre <a>
2025-06-24 14:11:48 +0900

    プロジェクト再生成

---
+++ src/Kadai1.java
@@ -0,0 +1,16 @@
+public class Kadai1 {
+    public static void main(String[] args) {
+        try {
+          int n = Integer.parseInt(args[0]);
+          for (int i = 1; i <= 1000; i++) {
+              System.out.print(i + " ");
+              if (i % n == 0) {
+                  System.out.println();
+              }
+          } catch (NumberFormatException) {
+                System.out.println("コマンドライン引数には数字を入力してください");
+
+            }
+                }
+            }
+        }
